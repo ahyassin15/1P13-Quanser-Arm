4bf8d1f5114aead7b81a5c0381694f7528d4c4dd
ahyassin15 <ah.yassin05@gmail.com>
2024-01-27 13:22:12 -0500

    Update

--- FinalQuanserCode.py
+++ FinalQuanserCode.py
@@ -1,135 +1,206 @@
-#!/usr/bin/env python
-# coding: utf-8
+ip_address = 'localhost' # Enter your IP Address here
+#--------------------------------------------------------------------------------
+import sys
+sys.path.append('../')
+from Common.simulation_project_library import *
 
-# In[ ]:
-
+hardware = False
+QLabs = configure_environment(project_identifier, ip_address, hardware).QLabs
+arm = qarm(project_identifier,ip_address,QLabs,hardware)
+potentiometer = potentiometer_interface()
+#---------------------------------------------------------------------------------
 
 import random
-        
+
+
+#function selects a random cage from the available cages, removes it from the list of cages, and spawns in the selected cage  
 def randomize_spawn(cages):
-    if not cages:
-        return cages, ["", ""]
 
-    cage_num = random.choice(cages)
-    cages.remove(cage_num)
-    arm.spawn_cage(cage_num)
+   #select a random cage number from the available spawnable cages
+   cage_num = random.choice(cages)
+   #remove selected cage from the list of cages that can spawn, ensuring that the same cage is not spawned again
+   cages.remove(cage_num)
+   #spawn the selected cage
+   arm.spawn_cage(cage_num)
 
-    cage_id_map = {
-        1: ["red", "small"],
-        2: ["green", "small"],
-        3: ["blue", "small"],
-        4: ["red", "large"],
-        5: ["green", "large"],
-        6: ["blue", "large"],
-    }
+   #map the cage number to its characteristics (color and size) using a predefined dictionary
+   cage_id_map = {
+       1: ["red", "small"],
+       2: ["green", "small"],
+       3: ["blue", "small"],
+       4: ["red", "large"],
+       5: ["green", "large"],
+       6: ["blue", "large"],
+   }
+   
+   #get the characteristics of the selected cage, defaulting to the placeholder list if not found
+   cage_id = cage_id_map.get(cage_num, ["", ""])
 
-    cage_id = cage_id_map.get(cage_num, ["", ""])
-    
-    return cages, cage_id
+   #update global variables cage_color and cage_size based on the selected cage. 
+   global cage_color
+   global cage_size
+   #initialize cage_color to the first index position of the cage_id list
+   cage_color = cage_id[0]
+   #initialize cage_size to the second index position of the cage_id list
+   cage_size = cage_id[1]
 
-def pick_up(position):
+   #if the argument passing through the function (cages) is empty
+   if not cages:
+       #return cages and a placeholder list to avoid errors in the case where there are no more cages to spawn
+       return cages, ["", ""]
+                       
+   #return updated list of cages and the characteristics of the spawned cage
+   return cages, cage_id
 
-    #move arm to cage spawn position
-    arm.move_arm(position[0],position[1],position[2])
-    time.sleep(0.5)
 
-    #close gripper
-    arm.control_gripper(45)
-    time.sleep(0.5)
+#function pick ups cage with the arm from its spawn position
+def pick_up(cage_spawn_position):
 
-    #arm moves to home position
-    arm.home()
-    time.sleep(0.5)
-    
+   #move arm to cage spawn position (the argument passed through the function)
+   arm.move_arm(cage_spawn_position[0],cage_spawn_position[1],cage_spawn_position[2])
+   #add delay for 2 seconds to allow the arm to reach the specified position
+   time.sleep(2)
+
+   #close gripper of the arm to grab the cage
+   arm.control_gripper(45)
+   #add delay for 2 seconds to allow the gripper to close securely around the cage
+   time.sleep(2)
+
+   #arm moves to home position after picking up the cage
+   arm.move_arm(0.406, 0.0, 0.483)
+   #add delay for 2 seconds to allow the arm to reach its home position
+   time.sleep(2)
+
+
+#function handles the rotation of the arm's base until it reaches the correct autoclave
 def rotate_arm_base():
-    x = 0
-    position_o = 0
-    
-    while x != 1:
-        position = (potentiometer.right()*100-50)*170/50
-        
-        if position != position_o:
-            arm.rotate_base(position-position_o)
-        
-        position_o = position
-    
-def drop_off(container_id):
-    
-    container_colour = container_id[0]
-    container_size = container_id[1]
-    arm.activate_autoclaves()
+  
+   #get initial reading from the right potentiometer
+   old_reading = potentiometer.right()
+  
+   #allow user to rotate the arm's base until it's at the correct autoclave
+   while arm.check_autoclave(cage_color) == False:
+ 
+       #get the new reading from the right potentiometer and scale it
+       new_reading = (potentiometer.right()*100-50)*170/50
+     
+       #if there's a change in the reading, rotate the arm's base accordingly
+       if new_reading != old_reading:
+           arm.rotate_base(new_reading-old_reading)
 
-    #set control variable as false
-    container_dropped_off = False
-    
-    while container_dropped_off:
-        
-        pLeft = potentiometer.left()
-        
-        #if left potentiometer value is > 0.5 and < 1.0 and the container size is small
-        if ((pLeft < 1.0) and (pLeft > 0.5)) and (container_size == "small"):
-            
-            #bring container to the top of the autoclave
-            arm.rotate_elbow(-15)
-            time.sleep(0.5)
-            
-            arm.rotate_shoulder(35)
-            time.sleep(0.5)
-            
-            #drop container inside autoclave
-            arm.control_gripper(-45)
-            time.sleep(0.5)
-           
-            #return arm home
-            arm.home()
-            time.sleep(0.5)
-            
-            dropped_off = True
-        
-        #if left potentiometer value is 1.0 and the container size is large
-        elif (pLeft == 1.0) and (container_size == "large"):
-            
-            #open the specfic coloured autoclave 
-            arm.open_autoclave(container_colour)
-            time.sleep(0.5)
-            
-            arm.rotate_elbow(30)
-            time.sleep(0.5)
-            
-            arm.rotate_shoulder(20)
-            time.sleep(0.5)
-            
-            arm.control_gripper(-45)
-            time.sleep(0.5)
-            
-            arm.home()
-            time.sleep(0.5)
-            
-            #close the specfic coloured autoclave
-            arm.open_autoclave(container_colour, False)
-            time.sleep(0.5)
-            
-            dropped_off = True
-    
-    arm.deactivate_autoclaves()
-    
+       #if there's no change in the reading, update the old reading for the next iteration in the while loop
+       else:
+           old_reading = new_reading
+ 
+
+#function drops off the cage at its corresponding autoclave
+def drop_off():
+
+   #activate autoclaves
+   arm.activate_autoclaves()
+   #initialize control variable as false
+   dropped_off = False 
+
+   while dropped_off == False:
+   
+       #if left potentiometer value is > 0.5 and < 1.0 and the cage size is small
+       if ((potentiometer.left() < 1.0) and (potentiometer.left() > 0.5)) and (cage_size == "small"):
+          
+           #add delay for 2 seconds
+           time.sleep(2)
+
+           #move arm to the position hovering over the specific colored autoclave based on the cage's color
+           if cage_color == 'red':
+               arm.move_arm(0.0,0.679,0.363)
+           elif cage_color == 'blue':
+               arm.move_arm(0.0,-0.679,0.363)
+           else:
+               #for green small-sized cage
+               arm.move_arm(-0.618,0.225,0.363)
+
+           #add delay for 2 seconds to allow the arm to reach the autoclave
+           time.sleep(2)
+     
+           #open arm's gripper to release the cage
+           arm.control_gripper(-20)
+           #add delay for 2 seconds to ensure the cage is fully released
+           time.sleep(2)
+
+           #return arm to home position
+           arm.home()
+           #add delay for 2 seconds to allow the arm to reach its home position
+           time.sleep(2)
+
+           #set control variable as true to exit out of the while loop
+           dropped_off = True
+   
+       #if left potentiometer value is 1.0 and the cage size is large
+       elif (potentiometer.left() == 1.0) and (cage_size == "large"):
+          
+           #add delay for 2 seconds
+           time.sleep(2)
+
+           #open the specific colored autoclave
+           arm.open_autoclave(cage_color)
+           #add delay for 2 seconds to allow autoclave to be fully opened
+           time.sleep(2)
+       
+           #move arm to the position hovering over the specific colored autoclave's drawer based on the cage's color
+           if cage_color == 'red':
+               arm.move_arm(0.0,0.428,0.194)
+           elif cage_color == 'blue':
+               arm.move_arm(0.0,-0.428,0.194)
+           else:
+               #for green large-sized cage
+               arm.move_arm(-0.446,0.162,0.204)
+           #add delay for 2 seconds to allow the arm to reach the autoclave
+           time.sleep(2)
+       
+           #open arm's gripper to release the cage
+           arm.control_gripper(-30)
+           #add delay for 2 seconds to ensure the cage is fully released
+           time.sleep(2)
+       
+           #return arm to home position
+           arm.home()
+           time.sleep(2)
+       
+           #close the specific coloured autoclave
+           arm.open_autoclave(cage_color, False)
+           #add delay for 2 seconds to allow the arm to reach its home position
+           time.sleep(2)
+
+           #set control variable as true to exit out of the while loop
+           dropped_off = True
+
+   #deactivate autoclaves
+   arm.deactivate_autoclaves()
+
+
+#main function utilizes all the functions in the program for spawning, picking up, rotating the arm base, and dropping off cages until all six cages have been processed
 def main():
 
-    #cage characteristics placeholder for colour in first index and size in second index
-    cage_id = ["",""]
-    cage_nums = [1,2,3,4,5,6]
-    
-    #position of cage spawn
-    cage_spawn_position = [-0.064,0.566,0.026]
-    
-    while (len(cage_nums) > 0):  
-        #Spawns in random cage and removes it from the cages able to spawn
-        cage_nums, cage_id = randomize_spawn(cage_nums)
-        
-        #Picks up the object
-        pick_up(cage_spawn_position)
-        rotate_arm_base()
-        drop_off(cage_id)
-        
-main()
+   #cage characteristics placeholder for color in first index and size in second index
+   cage_id = ["",""]
+   #initialize list with the numbers of available cages that can be spawned
+   cage_nums = [1,2,3,4,5,6]
+ 
+   #position of cage spawn
+   cage_spawn_position = [0.617, 0.054, 0.044]
+ 
+   #continue the loop until there are no more cages left to spawn
+   while (len(cage_nums) > 0):
+       
+       #spawn in random cage and remove it from the list of cages able to spawn
+       cage_nums, cage_id = randomize_spawn(cage_nums)
+     
+       #pick up the cage from the spawn position
+       pick_up(cage_spawn_position)
 
+       #rotate the arm base until it reaches correct autoclave
+       rotate_arm_base()
+
+       #drop off the cage into the autoclave
+       drop_off()
+
+main()
